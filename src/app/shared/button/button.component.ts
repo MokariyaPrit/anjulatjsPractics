fc971982ff2ee76201c4ac565cc5e91cccdb2de0
prit mokariya <mokariyaprit1123@gmail.com>
2025-09-04 16:44:50 +0530

    update css and add one more shared control

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+  btnName = input.required()
+  btnSymbol = input()
+}
